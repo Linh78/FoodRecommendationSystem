0589a0b39acd0dbff04d8499376421b3445da3fb
Linh <thuylinh7801@gmail.com>
2023-04-21 04:23:34 +0700

    tips and tricks

--- RecommendFood/Controllers/IntroduceController.cs
+++ RecommendFood/Controllers/IntroduceController.cs
@@ -11,6 +11,35 @@ namespace RecommendFood.Controllers
         {
             List<News> news = new List<News>();
             news = dBContext.News.ToList();
+            List<News> newsRandom = new List<News>();
+            newsRandom = dBContext.News.OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+            ViewBag.newsRandom = newsRandom;
+
+            List<News> newsRandom1 = new List<News>();
+            newsRandom1 = dBContext.News.OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+            ViewBag.newsRandom1 = newsRandom1;
+
+            return View(news);
+        }
+        
+        //GET: News/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var news = dBContext.News.FirstOrDefault(x => x.Id == id);
+            var listnews = dBContext.News.ToList();
+            ViewBag.listnews = listnews;
+            return View(news);
+        }
+
+        //GET: News
+        public IActionResult TipsTrick()
+        {
+            List<News> news = new List<News>();
+            news = dBContext.News.ToList();
             return View(news);
         }
     }
---
+++ RecommendFood/Controllers/RecommendController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecommendFood.Controllers
+{
+    public class RecommendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
